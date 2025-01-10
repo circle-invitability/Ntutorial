1c4c168ca3d5f1bb58141a847444a18ab09fb24b
Luman <engineersoftware283@gmail.com>
2025-01-10 14:56:12 +0000

    message

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,7 +1,7 @@
 const {readFileSync, writeFileSync} = require('fs');
 
 
-
+console.log('start')
 const first = readFileSync('./content/first.txt', 'utf8');
 const second = readFileSync('./content/second.txt', 'utf8');
 
@@ -11,6 +11,9 @@ writeFileSync (
     {flag: 'a'} // this flag will append the data to the file if it exists
     
 )
+console.log('done with this task')
+
+console.log('starting the next one')
 /* by the default the writeFileSync will overwrite the file if it exists, but if we want to append the data to the file we can use the flag option in the writeFileSync method. the flag option is an object that takes a key value pair. the key is flag and the value is a string 'a' which means append. so the data will be appended to the file if it exists. if the file does not exist it will be created and the data will be written to it.
 
 syntax writeFileSync ;
--- app.js
+++ app.js
@@ -1,39 +1,14 @@
-const { error } = require('console');
-const {readFile, writeFile} = require('fs');
-
-
-
-
-readFile('./content/first.txt', 'utf8', (err, result) => {
-        if(err){
-            console.log(err);
-            return;
-
-        }
-    const first = result;
-
-    readFile('./content/second.txt', 'utf-8', (err, result) => {
-            if(err){
-                console.log(err);
-                return;
-        
-            }
-            const second = result; 
-            writeFile(
-                './content/result-async.txt',
-                `here is the result: ${first}, ${second}`,
-                (err, result) => {
-                    if (err){
-                        console.log(`an error occured: $(err)`);
-                    }
-                    console.log(result);
-                }
-            )
+const http = require('http');
+//the http module allow us to setup a web server
+try {
+    const server = http.createServer((req, res) => {
+        res.write("welcome to my homepage");
+    });
     
-        });
+    server.listen(8010);
     
-    })
-        
-
+} catch (error) {
+    console.log(error)
     
+};
 
